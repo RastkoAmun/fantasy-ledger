1b87092939b265667b6471a104d8437d32bb4264
RastkoAmun <raciosh@gmail.com>
2025-01-25 16:03:31 -0800

    feat: creating Tab Panel on the Dialog Form for adding a new character

---
+++ client-side/src/components/Dialogs/CharacterCreation/DialogForm.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Box, Dialog, Tabs, Tab } from "@mui/material";
+
+const tabLabels = {
+  main: {
+    core: "Core",
+    class: "Class & Race",
+    scores: "Ability Scores",
+    features: "Features",
+  },
+  finish: "Finish",
+};
+
+const CharacterCreationDialog = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+      <Dialog open={true} maxWidth={false}>
+        <Box borderBottom={1}>
+          <Tabs
+            value={value}
+            sx={{
+              "& .MuiTabs-indicator": {
+                display: "none",
+              },
+            }}
+          >
+            {Object.entries(tabLabels.main).map(([key, value], index) => (
+              <Tab
+                key={key}
+                label={value}
+                onClick={() => setValue(index)}
+                sx={{
+                  width: 155,
+                  borderRight: 1,
+                  "&.Mui-selected": {
+                    backgroundColor: "teal",
+                    color: "white",
+                  },
+                }}
+              />
+            ))}
+            <Tab
+              label={tabLabels.finish}
+              onClick={() => setValue(4)}
+              color="green"
+              sx={{
+                width: 180,
+                "&.Mui-selected": {
+                  backgroundColor: "teal",
+                  color: "white",
+                },
+              }}
+            />
+          </Tabs>
+        </Box>
+      </Dialog>
+    </>
+  );
+};
+
+export default CharacterCreationDialog;
