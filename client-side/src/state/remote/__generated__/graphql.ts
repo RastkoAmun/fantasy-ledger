cd65ba2b2468b249cd2aa159c9098d97f3136930
Rastko Salvarica <raciosh@gmail.com>
2025-02-27 13:04:51 -0800

    feat: updating types on ApolloServer and types on ApolloClient

--- client-side/src/state/remote/__generated__/graphql.ts
+++ client-side/src/state/remote/__generated__/graphql.ts
@@ -26,6 +26,23 @@ export type AbilityScores = {
   wisdom: Scalars['Int']['output'];
 };
 
+export type Character = {
+  __typename?: 'Character';
+  abilityScores: Scalars['Int']['output'];
+  class?: Maybe<Scalars['String']['output']>;
+  currentHealth?: Maybe<Scalars['Int']['output']>;
+  hitDice?: Maybe<Scalars['String']['output']>;
+  id: Scalars['Int']['output'];
+  level: Scalars['Int']['output'];
+  maxHealth?: Maybe<Scalars['Int']['output']>;
+  name: Scalars['String']['output'];
+  race?: Maybe<Scalars['String']['output']>;
+  speed?: Maybe<Scalars['Int']['output']>;
+  subclass?: Maybe<Scalars['String']['output']>;
+  subrace?: Maybe<Scalars['String']['output']>;
+  temporaryHealth?: Maybe<Scalars['Int']['output']>;
+};
+
 export type CreateAbilityScoresInput = {
   charisma: Scalars['Int']['input'];
   constitution: Scalars['Int']['input'];
@@ -35,14 +52,34 @@ export type CreateAbilityScoresInput = {
   wisdom: Scalars['Int']['input'];
 };
 
+export type CreateCharacterInput = {
+  abilityScores: Scalars['Int']['input'];
+  class: Scalars['String']['input'];
+  currentHealth: Scalars['Int']['input'];
+  hitDice: Scalars['String']['input'];
+  level: Scalars['Int']['input'];
+  maxHealth: Scalars['Int']['input'];
+  name: Scalars['String']['input'];
+  race: Scalars['String']['input'];
+  subclass: Scalars['String']['input'];
+  subrace: Scalars['String']['input'];
+  temporaryHealth: Scalars['Int']['input'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
   createAbilityScores: AbilityScores;
+  createCharacter?: Maybe<Character>;
 };
 
 
 export type MutationCreateAbilityScoresArgs = {
   input: CreateAbilityScoresInput;
+};
+
+
+export type MutationCreateCharacterArgs = {
+  input: CreateCharacterInput;
 };
 
 export type Query = {
--- server-side/schema/typeDefs.js
+++ server-side/schema/typeDefs.js
@@ -4,7 +4,8 @@ export const typeDefs =`#graphql
   }
 
   type Mutation {
-    createAbilityScores(input: CreateAbilityScoresInput!): AbilityScores!
+    createAbilityScores(input: CreateAbilityScoresInput!): AbilityScores!,
+    createCharacter(input: CreateCharacterInput!): Character
   }
 
   type AbilityScores {,
@@ -17,6 +18,22 @@ export const typeDefs =`#graphql
     charisma: Int!
   }
 
+  type Character {
+    id: Int!,
+    name: String!,
+    level: Int!,
+    currentHealth: Int,
+    maxHealth: Int,
+    temporaryHealth: Int,
+    hitDice: String,
+    race: String,
+    subrace: String,
+    class: String,
+    subclass: String,
+    speed: Int,
+    abilityScores: Int!
+  }
+
   input CreateAbilityScoresInput {
     strength: Int!,
     dexterity: Int!,
@@ -26,8 +43,17 @@ export const typeDefs =`#graphql
     charisma: Int!
   }
 
-  # input UpdateAbilityScoresInput {
-  #   id: Int!,
-  #   ...CreateAbilityScoresInput
-  # }
+  input CreateCharacterInput {
+    name: String!,
+    level: Int!,
+    currentHealth: Int!,
+    maxHealth: Int!,
+    temporaryHealth: Int!,
+    hitDice: String!,
+    race: String!,
+    subrace: String!,
+    class: String!,
+    subclass: String!,
+    abilityScores: Int!
+  }
 `
