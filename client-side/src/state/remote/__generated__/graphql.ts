fbc2e41ea5a6b443e41ceb5c03f7027cf49de67f
RastkoAmun <raciosh@gmail.com>
2025-02-26 16:24:14 -0800

    feat: adding configuration and generating types

---
+++ client-side/src/state/remote/__generated__/graphql.ts
@@ -0,0 +1,51 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type AbilityScores = {
+  __typename?: 'AbilityScores';
+  charisma: Scalars['Int']['output'];
+  constitution: Scalars['Int']['output'];
+  dexterity: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  intelligence: Scalars['Int']['output'];
+  strength: Scalars['Int']['output'];
+  wisdom: Scalars['Int']['output'];
+};
+
+export type CreateAbilityScoresInput = {
+  charisma: Scalars['Int']['input'];
+  constitution: Scalars['Int']['input'];
+  dexterity: Scalars['Int']['input'];
+  intelligence: Scalars['Int']['input'];
+  strength: Scalars['Int']['input'];
+  wisdom: Scalars['Int']['input'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createAbilityScores: AbilityScores;
+};
+
+
+export type MutationCreateAbilityScoresArgs = {
+  input: CreateAbilityScoresInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  abilityScores: AbilityScores;
+};
