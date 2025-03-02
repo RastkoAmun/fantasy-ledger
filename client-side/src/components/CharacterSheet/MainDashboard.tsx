3021bf29906c0de60712052bd5c86571fbe09c62
Rastko Salvarica <raciosh@gmail.com>
2025-03-02 15:20:12 -0800

    feat: adding character info to dashboard

--- client-side/src/components/CharacterSheet/MainDashboard.tsx
+++ client-side/src/components/CharacterSheet/MainDashboard.tsx
@@ -16,8 +16,36 @@ const MainDashboard = () => {
       >
         <Grid item height="100%" xs={4}>
           <Stack direction="column" bgcolor="teal" height="100%" width="100%">
-            <Box height="20%" bgcolor="red" margin={2}>
-              Placeholder
+          <Box
+              height="20%"
+              border={1}
+              borderColor="purple"
+              margin={2}
+              mb={0}
+              bgcolor="#9A9696"
+              boxSizing="border-box"
+            >
+              <Stack direction="row" justifyContent="space-between">
+                <Stack direction="row">
+                  <Box width={80} height={80} m={1} bgcolor="blue">
+                    <Image
+                      src="/images/ChImage.png"
+                      alt="character-picture"
+                      width={80}
+                      height={80}
+                    />
+                  </Box>
+                  <Stack mt={1}>
+                    <Typography>Eryarus</Typography>
+                    <Typography>Level: 1</Typography>
+                    <Typography>Male / Half-Elf</Typography>
+                  </Stack>
+                </Stack>
+                <Stack mr={3} justifyContent='center'>
+                  <Typography>Gloomstalker</Typography>
+                  <Typography>Ranger</Typography>
+                </Stack>
+              </Stack>
             </Box>
             <AbilityScoresDashboard />
             <Box height="20%" bgcolor="red" margin={2}>
